e1a09d41410457aaefb7a2cb12192fec7b84f4de
gabriel_abreu <63174449+GabrielAbreu571@users.noreply.github.com>
2023-06-19 20:01:23 -0300

    Add files via upload

---
+++ eletronica/JogoDeNave/JogoDeNave.ino
@@ -0,0 +1,17 @@
+/* 
+  Projeto criado para a disciplina eletrônica para computação
+  Nome dos integrantes:
+    Gabriel de Andrade Abreu - NUMUSP: 14571362
+    Guilherme - NUMUSP: 
+    Isabela - NUMUSP: 
+*/
+
+
+void setup() {
+  
+
+}
+
+void loop() {
+  
+}
